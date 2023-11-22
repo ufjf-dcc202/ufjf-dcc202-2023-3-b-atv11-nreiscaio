999d2a53696c4f9f3716187d37dcecd1dc48f09c
Caio Nascimento <nreis.caio@gmail.com>
2023-11-21 22:15:03 -0300

    seleciona elementos

---
+++ main.js
@@ -0,0 +1,10 @@
+const pEntrada = document.querySelector.Selector("#entrada");
+const btnAdicionar = document.querySelector.Selector("#adicionar");
+const btnLimpar = document.querySelector.Selector("#limpar");
+const olItens = document.querySelector.Selector("#itens");
+
+const li = document.createElement("li");
+li.textContent = "Primeiro";
+console.log(li);
+
+olItens.appendChild(li);
