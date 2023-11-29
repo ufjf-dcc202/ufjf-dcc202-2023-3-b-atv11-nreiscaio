21646653244b6b66959013bf0c2cf04aa047c3e6
Caio Nascimento <nreis.caio@gmail.com>
2023-11-28 21:18:29 -0300

    finalizacao

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
+import {adicionaNaLista, getLista, limpaLista} from "./src/lista.js";
+
 const pEntrada = document.querySelector("#entrada");
 const btnAdicionar = document.querySelector("#adicionar");
 const btnLimpar = document.querySelector("#limpar");
 const olItens = document.querySelector("#itens");
 
-adicionaElementoNaLista("Primeiro")
-adicionaElementoNaLista("Segundo")
-adicionaElementoNaLista("Terceiro")
+btnAdicionar.addEventListener('click', adicionaItemDaEntrada);
+btnLimpar.addEventListener('click', limparListaOrdenada);
 
+function limparListaOrdenada(){
+    limpaLista();
+    atualizarListaOrdenada();
+}
 
-function adicionaElementoNaLista(texto){
+function adicionaItemDaEntrada(){
+    const valor = pEntrada.textContent;
+    adicionaNaLista(valor);
+    pEntrada.textContent="";
+    atualizarListaOrdenada();
+}
+
+function atualizarListaOrdenada(){
+    const lista = getLista();
+    olItens.innerHTML = "";
+    for(let i = 0; i < lista.length; i++){
+        adicionaElementoNaListaOrdenada(lista[i]);
+    }
+}
+
+function adicionaElementoNaListaOrdenada(texto){
     const li = document.createElement("li");
     li.textContent = texto;
     olItens.appendChild(li);
